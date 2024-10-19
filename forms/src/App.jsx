46b6cf6c674c0bc024b112e843d4b739cf32f370
DEVita42 <dvt.lorenzo@gmail.com>
2024-10-19 10:47:44 +0200

    Controlled Forms - 2

--- forms/src/App.jsx
+++ forms/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import InteractiveWelcome from './InteractiveWelcome'
+import Login from './Login'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <InteractiveWelcome></InteractiveWelcome>
+      <Login></Login>
     </>
   )
 }
---
+++ forms/src/Login.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [remember, setRemember] = useState(false);
+
+  const Username = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const Password = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const Remember = (e) => {
+    setRemember(e.target.checked);
+  };
+
+  const Submit = (e) => {
+    e.preventDefault();
+    console.log('Username:', username);
+    console.log('Password:', password);
+    console.log('Remember Me:', remember);
+  };
+
+  return (
+    <form onSubmit={Submit}>
+      <div>
+        <label>
+          Username:
+          <input 
+            type="text" 
+            value={username} 
+            onChange={Username} 
+            placeholder="Enter username" 
+          />
+        </label>
+      </div>
+
+      <div>
+        <label>
+          Password:
+          <input 
+            type="password" 
+            value={password} 
+            onChange={Password} 
+            placeholder="Enter password" 
+          />
+        </label>
+      </div>
+
+      <div>
+        <label>
+          <input 
+            type="checkbox" 
+            checked={remember} 
+            onChange={Remember} 
+          />
+          Remember Me
+        </label>
+      </div>
+
+      <div>
+        <button type="submit">Login</button>
+      </div>
+    </form>
+  );
+}
+
+export default Login;
